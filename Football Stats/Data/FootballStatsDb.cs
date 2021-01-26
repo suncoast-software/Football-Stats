4ecc3a54315a636d5e147092617f62067e145b05
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-01-26 18:35:44 -0500

    Add project files.

---
+++ Football Stats/Data/FootballStatsDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Football_Stats.Data
+{
+    public class FootballStatsDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Password=451145_Gl;User Id=postgres;Database=footballStatsDb");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Football Stats/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Stats.Models
+{
+   public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
